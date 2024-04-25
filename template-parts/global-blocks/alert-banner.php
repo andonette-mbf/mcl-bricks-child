52390bb5c4a1be51590dff4a2d55df2f315a3fdb
andonette-mbf <94548512+andonette-mbf@users.noreply.github.com>
2024-04-25 10:02:53 +0100

    Deleted irrelevant files

--- template-parts/global-blocks/alert-banner.php
+++
@@ -1,9 +0,0 @@
-<div class="container-fluid alert-banner-container">
-    <div class="container">
-        <div class="row">
-            <div class="col-12 anime-view">
-                <span class="title to-animate"><?php the_field('alert_banner_text', 'options');?></span>
-            </div>
-        </div>
-    </div>
-</div>
