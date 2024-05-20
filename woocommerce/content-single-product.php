c5cce4fb05a1101041830433248e51360dc9e898
andonette-mbf <94548512+andonette-mbf@users.noreply.github.com>
2024-05-20 14:04:15 +0100

    Refactor tabs

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -512,42 +512,8 @@ if ( post_password_required () ) {
   <?php } ?>
 <?php } ?>
 
-<!--tabbed data -->
-<section class="brxe-section brxe-wc-section">
-<div class="brxe-container">
-  <div id="brxe-tabs" data-script-id="tabs" class="brxe-tabs-nested">
-<div id="brxe-pljtos" class="brxe-block tab-menu">
-<div class="brxe-div tab-title brx-open">
-<div class="brxe-text-basic">Course Details</div>
-</div>
-<div class="brxe-div tab-title">
-<div class="brxe-text-basic">Included In Course</div>
-</div>
-<div class="brxe-div tab-title">
-<div class="brxe-text-basic">Pre Training Requirements</div>
-    </div>
-</div>
-    <div class="brxe-block tab-content">
-
-              <div class="brxe-block tab-pane fade active show brx-open">
-<div class="brxe-text">
-    <p><?php the_field ( 'course_details' ); ?></p>
-</div>
-</div>
-<div class="brxe-block tab-pane">
-<div class="brxe-text">
-<p><?php the_field ( 'included_in_course' ); ?></p>
-</div>
-</div>
-<div class="brxe-block tab-pane">
-<div class="brxe-text">
-<p><?php the_field ( 'pre_training_requirements' ); ?></p>
-    </div>
-</div>
-</div>
-</div>
-</div>
-</section>
+<!--tabs partial -->
+<?php get_template_part('woocommerce/template-parts/template', 'tabs'); ?>
 <section class="brxe-section brxe-wc-section related-products">
 <div class="related-title">
 <?php
