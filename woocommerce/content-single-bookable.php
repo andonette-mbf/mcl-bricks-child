a78514416fba6cfc0490bc5eb3618c9af374cdf9
andonette-mbf <94548512+andonette-mbf@users.noreply.github.com>
2024-07-28 18:23:26 +0100

    refactor course full block

--- woocommerce/content-single-bookable.php
+++ woocommerce/content-single-bookable.php
@@ -457,7 +457,7 @@ if ( ! empty ( $bookings ) ) {
                 </div>
               </div>
 
-              <!--Choose number-->
+              <!--People select-->
 
               <div class="course-step" id="step-3">
                 <div class="step-title">
@@ -618,16 +618,7 @@ if ( ! empty ( $bookings ) ) {
                     class="cta-button float-right w-auto" style="display: none;"></a>
                 </div>
               <?php else : ?>
-                <div class="brxe-block mcl-dates">
-                  <div class="brxe-div mcl-flex--col">
-                    <h3 class="brxe-heading mcl-dates__heading">Course fully booked. please try another venue or call us
-                      for upcoming dates
-                    </h3>
-                  </div>
-                  <div class="brxe-div mcl-flex--col">
-                    <a class="brxe-button mcl-btn--white mcl-btn bricks-button" href="#contact">Contact Us</a>
-                  </div>
-                </div>
+                <?php get_template_part ( 'woocommerce/template-parts/block', 'full' ); ?>
               <?php endif; ?>
 
             </div>
---
+++ woocommerce/template-parts/block-full.php
@@ -0,0 +1,10 @@
+<div class="brxe-block mcl-dates">
+  <div class="brxe-div mcl-flex--col">
+    <h3 class="brxe-heading mcl-dates__heading">Course fully booked. please try another venue or call us
+      for upcoming dates
+    </h3>
+  </div>
+  <div class="brxe-div mcl-flex--col">
+    <a class="brxe-button mcl-btn--white mcl-btn bricks-button" href="#contact">Contact Us</a>
+  </div>
+</div>
