dd08be6eaa5e0f4f67beec4e3bd796331b982ea7
andonette-mbf <94548512+andonette-mbf@users.noreply.github.com>
2024-07-28 17:16:02 +0100

    create a new file for common variables

---
+++ woocommerce-variables.php
@@ -0,0 +1,7 @@
+<?php
+// @suppress PHP0415
+// Use the constants defined in wp-config.php
+$consumer_key    = WC_BOOKINGS_CONSUMER_KEY;
+$consumer_secret = WC_BOOKINGS_CONSUMER_SECRET;
+$api_url         = WC_BOOKINGS_API_URL . 'wp-json/wc-bookings/v1/bookings';
+$site_url        = 'https://mcl.local';
--- woocommerce/content-single-bookable.php
+++ woocommerce/content-single-bookable.php
@@ -5,13 +5,9 @@
  */
 defined ( 'ABSPATH' ) || exit;
 global $product;
+include get_stylesheet_directory () . '/woocommerce-variables.php';
 
-// @suppress PHP0415
-// Use the constants defined in wp-config.php
-$consumer_key    = WC_BOOKINGS_CONSUMER_KEY;
-$consumer_secret = WC_BOOKINGS_CONSUMER_SECRET;
-$api_url         = WC_BOOKINGS_API_URL . 'wp-json/wc-bookings/v1/bookings';
-$site_url        = 'https://mcl.local';
+
 
 // Initialize common variables
 $product_group_id = $product->is_type ( 'booking' ) ? parent_grouped_id ( $product->get_id () ) : $product->get_id ();
