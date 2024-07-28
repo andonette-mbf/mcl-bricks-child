ff668ff702d42bf478a9935467cb221281074c82
andonette-mbf <94548512+andonette-mbf@users.noreply.github.com>
2024-07-28 20:26:25 +0100

    block date create file

--- woocommerce/content-single-bookable.php
+++ woocommerce/content-single-bookable.php
@@ -7,16 +7,13 @@ defined ( 'ABSPATH' ) || exit;
 global $product;
 include get_stylesheet_directory () . '/woocommerce-variables.php';
 
-
 /**
  * @suppress PHP0417
  */
-
-//GET THE DATES
+//Get available dates from booking 
 $availabilityInFuture = false;
 $availability         = get_post_meta ( $product->get_id (), '_wc_booking_availability' );
 $availabilityTest     = array_filter ( $availability );
-
 // Store availability dates
 $futureAvailabilityDates = [];
 $current_date            = new DateTime();
@@ -318,7 +315,77 @@ if ( ! empty ( $bookings ) ) {
                 </div>
 
                 <div class="step-layouts">
+                  <div class="layouts" id="layout-list">
+                    <div class="calendar-list">
 
+                      <?php
+                      // Assuming $future_availability_rows is fetched and contains data
+// Fetch the manual_dates repeater field data for the current product
+                      $manual_dates          = get_field ( 'manual_dates' );
+                      $available_spaces_list = [];
+
+                      // Create a list of available spaces in order and check the dates
+                      $current_date = date ( 'Ymd' ); // Get the current date in 'YYYYMMDD' format
+                    
+                      if ( $manual_dates ) {
+                        foreach ( $manual_dates as $row ) {
+                          $start_date = $row[ 'start_date' ]; // Assuming 'start_date' is in 'YYYYMMDD' format
+                          if ( $start_date >= $current_date ) { // Check if the start date is today or in the future
+                            $available_spaces_list[] = $row[ 'available_spaces' ];
+                            }
+                          }
+                        }
+                      ?>
+
+                      <div class="table-section">
+                        <?php if ( ! empty ( $future_availability_rows ) ) : ?>
+                          <table class="table">
+                            <style>
+                              /* CSS to hide columns on mobile devices */
+                              @media only screen and (max-width: 767px) {
+                                .mobile-hide {
+                                  display: none;
+                                }
+                              }
+                            </style>
+                            <thead>
+                              <tr>
+                                <th>Start Date</th>
+                                <th class="mobile-hide">Location</th>
+                                <th>Availability</th>
+                                <th class="add-td"></th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <?php
+                              $i = 0; // Initialize a counter to access available spaces
+                              foreach ( $future_availability_rows as $row ) :
+                                $available_spaces = isset ( $available_spaces_list[ $i ] ) ? $available_spaces_list[ $i ] : 'N/A';
+                                $i++; // Increment the counter
+                                ?>
+                                <tr class="availability-date <?php echo esc_attr ( $row[ 'hidden_class' ] ); ?>"
+                                  data-start="<?php echo esc_attr ( $row[ 'from_date' ] ); ?>"
+                                  data-end="<?php echo esc_attr ( $row[ 'to_date' ] ); ?>">
+                                  <td><?php echo esc_html ( $row[ 'from_date' ] ); ?></td>
+                                  <td class="mobile-hide"><?php echo esc_html ( $row[ 'location_name' ] ); ?></td>
+                                  <td><?php echo esc_html ( $available_spaces ); ?><?php echo esc_html ( $result_count ); ?>
+                                  </td>
+                                  <td class="add-td">
+                                    <a href="#" data-day="<?php echo esc_attr ( $row[ 'data_day' ] ); ?>"
+                                      data-month="<?php echo esc_attr ( $row[ 'data_month' ] ); ?>"
+                                      data-year="<?php echo esc_attr ( $row[ 'data_year' ] ); ?>"
+                                      class="cta-button book-now-button float-right">Select Date</a>
+                                  </td>
+                                </tr>
+                              <?php endforeach; ?>
+                            </tbody>
+                          </table>
+                          <button id="show-more-dates" class="cta-button" style="display: none;">More</button>
+                        <?php endif; ?>
+                      </div>
+
+                    </div>
+                  </div>
                   <div class="layouts float-left w-100 position-relative" id="layout-calendar">
                     <?php do_action ( 'woocommerce_single_product_summary' ); ?>
                   </div>
---
+++ woocommerce/template-parts/block-dates.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying dates
+ */
+defined ( 'ABSPATH' ) || exit;
+global $product;
+include get_stylesheet_directory () . '/woocommerce-variables.php';
+/**
+ * @suppress PHP0417
+ */
+?>
